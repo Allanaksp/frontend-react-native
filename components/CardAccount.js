1e795e1024cfe9cd77b871d8f6b899230f543f24
Allanaksp <karolineallana8@gmail.com>
2024-08-15 17:50:24 -0300

    cardAccount2

--- components/CardAccount.js
+++ components/CardAccount.js
@@ -3,10 +3,14 @@ import { Image } from 'expo-image'
 
 export default function CardAccount () {
     return(
-        <view style={styles.card}>
-            <image
+        <View style={styles.card}>
+            <Image
                 style={styles.logo}
-                source=
+                source="https://mailmeteor.com/logos/assets/PNG/Gmail_Logo_512px.png" />
+            <View style={styles.content}>
+                <Text style={styles.service}>Gmail</Text>
+                <Text style={styles.username}>karolineallana17@gmail.com</Text>
+            </View>
+        </View>
     )
-        </view>
 }
